0209888dab87b37cec2396fd3ad42e799d169bc7
Jodan Lima Galas <jodan.galas@fatec.sp.gov.br>
2020-03-04 21:10:24 -0300

    Add files via upload

---
+++ Questão 02.c
@@ -0,0 +1,8 @@
+//Faça um programa que solicite um número (inteiro/positivo) e imprima-o.
+#include <stdio.h>
+main(){
+	int a;
+	printf("Informe um numero: ");
+	scanf("%d", &a);
+	printf("O numero digitado e: %d", a);
+}
---
+++ Questão 11.c
@@ -0,0 +1,14 @@
+//Dado um número inteiro positivo n, calcular a soma dos n primeiros números naturais.
+#include <stdio.h>
+main(){
+	
+	int n, i, soma;
+	printf("Informe um numero: ");
+	scanf("%d", &n);
+	soma = 0;
+	for (i=1; i<= n; i++){
+		soma+=i;
+	}
+	printf("A soma dos %d primeiros numeros naturais e: %d",n, soma );
+	
+}
