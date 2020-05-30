465af474e93df6b048fa2b36440a34d800aa0b3f
JodanGalas <jodan.galas@fatec.sp.gov.br>
2020-05-29 21:28:13 -0300

    Create Questão 02.c

---
+++ Prática 05/Questão 02.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+# include <string.h>
+
+int palindromo(char string[51]) {
+
+    int count = 0;
+    for (int decrescente = strlen(string), crescente = 0; decrescente != 0; decrescente --, crescente ++) {
+        if (string[crescente] != string[decrescente -1]) {
+            count ++;
+
+        }
+
+    }
+
+    return (count == 0) ? 1 : 0;
+
+}
+
+
+int main(void) {
+
+    char string[51];
+
+    printf("Digite algo: ");
+    gets(string);
+
+    printf("'%s'%s é um palíndromo.\n", string, (palindromo(string) == 1) ? "" : " não");
+
+    linha();
+
+    return 0;
+
+}
